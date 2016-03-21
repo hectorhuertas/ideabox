72a7d170723f048330d03f9512ed801b214a69a6
Hector Huertas <hectorhuertas@gmail.com>
2016-03-21 17:46:31 -0600

    Implement index, create and destroy for ideas api

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,5 @@
 class Idea < ActiveRecord::Base
   enum quality: %w(swill plausible genius)
+
+  scope :by_recently_updated, -> {order("updated_at desc")}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :ideas, only: [:index, :create, :destroy]
+    end
+  end
 end
