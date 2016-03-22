76bbe53a238e1ba1d0d58073858dc66f91bc9bfc
Hector Huertas <hectorhuertas@gmail.com>
2016-03-22 15:41:05 -0600

    Add model tests

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -6,4 +6,52 @@ class IdeaTest < ActiveSupport::TestCase
 
     assert_equal 'swill', idea.quality
   end
+
+  test "upvote a swill idea" do
+    idea = Idea.create()
+
+    idea.upvote
+
+    assert_equal 'plausible', idea.quality
+  end
+
+  test "upvote a plausible idea" do
+    idea = Idea.create(quality: 1)
+
+    idea.upvote
+
+    assert_equal 'genius', idea.quality
+  end
+
+  test "upvote a genius idea does nothing" do
+    idea = Idea.create(quality: 2)
+
+    idea.upvote
+
+    assert_equal 'genius', idea.quality
+  end
+
+  test "downvote a swill idea does nothing" do
+    idea = Idea.create()
+
+    idea.downvote
+
+    assert_equal 'swill', idea.quality
+  end
+
+  test "downvote a plausible idea" do
+    idea = Idea.create(quality: 1)
+
+    idea.downvote
+
+    assert_equal 'swill', idea.quality
+  end
+
+  test "downvote a genius idea" do
+    idea = Idea.create(quality: 2)
+
+    idea.downvote
+
+    assert_equal 'plausible', idea.quality
+  end
 end
