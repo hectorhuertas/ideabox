3ca0727570684b6b7046e3a3b1afc8b982ea818a
Hector Huertas <hectorhuertas@gmail.com>
2016-03-26 02:49:05 -0600

    Move inline edition to Idea module

--- app/assets/javascripts/idea.js
+++ app/assets/javascripts/idea.js
@@ -69,10 +69,49 @@ var Idea = (function(){
      .then(Tag.refreshAll())
   }
 
+  var editBody = function(e){
+    var body = $(e.target).closest('.idea').data('full-body')
+     var xx = $(e.target).replaceWith('<textarea class="form-control" id="idea-body-editor">' + body + '</textarea>')
+     $('#idea-body-editor').height($('#idea-body-editor').prop('scrollHeight'));
+     $('#idea-body-editor').focus()
+  }
+
+  var updateBody = function(e){
+    var body = $('#idea-body-editor').val()
+    var id = $(e.target).closest('.idea').data('id')
+    var tags = $(e.target).closest('.idea').data('tags').split(" ").filter(function(w){return w})
+    var idea = {body: body, tags: tags}
+    // debugger
+    $.ajax({
+      type: 'PATCH',
+      url: '/api/v1/ideas/' + id,
+      data: {idea: idea}
+    }).success(reload(id))
+  }
+
+  var updateTitle = function(e){
+    var title = e.target.textContent
+    // var id = e.target.parentElement.parentElement.parentElement.parentElement.dataset.id
+    var id = $(e.target).closest('.idea').data('id')
+    var tags = $(e.target).closest('.idea').data('tags').split(" ").filter(function(w){return w})
+    var idea = {title: title, tags: tags}
+
+    $.ajax({
+      type: 'PATCH',
+      url: '/api/v1/ideas/' + id,
+      data: {idea: idea}
+    }).success(reload(id))
+  // debugger
+  }
+
+
   return {
     addAll: refreshAll,
     create: create,
     vote: vote,
     destroy: destroy,
+    editBody: editBody,
+    updateBody: updateBody,
+    updateTitle: updateTitle,
   }
 })();
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,75 +1,15 @@
 $(document).ready(function(){
   Idea.addAll()
   $('#save-idea').on('click', Idea.create)
-  $('#idea-box').delegate('button.deleter',   'click', Idea.destroy)
   $('#idea-box').delegate('button.voter', 'click', Idea.vote)
+  $('#idea-box').delegate('button.deleter',   'click', Idea.destroy)
+
+  $('#idea-box').delegate('span.body', 'click', Idea.editBody)
+  $('#idea-box').delegate('span.title', 'focusout', Idea.updateTitle)
+  $('#idea-box').delegate('#idea-body-editor', 'focusout', Idea.updateBody)
+
   $('#sort-by-quality').on('click', Sorter.byQuality)
   $('#fuzzy-filter').on('keyup', Filter.byContent)
   $('#show-all').on('click', Filter.clear)
   $('#tag-list').delegate('button', 'click', Filter.byTag)
-
-  $('#idea-box').delegate('span.title', 'focusout', updateTitle)
-  $('#idea-box').delegate('span.body', 'click', editBody)
-  $('#idea-box').delegate('#idea-body-editor', 'focusout', updateBody)
 })
-
-function refreshIdeas(){
-  $('#idea-box').empty()
-  Idea.addAll()
-}
-
-function editBody(e){
-  var body = $(e.target).closest('.idea').data('full-body')
-   var xx = $(e.target).replaceWith('<textarea class="form-control" id="idea-body-editor">' + body + '</textarea>')
-   $('#idea-body-editor').height($('#idea-body-editor').prop('scrollHeight'));
-   $('#idea-body-editor').focus()
-}
-
-function updateBody(e){
-  var body = $('#idea-body-editor').val()
-  var id = $(e.target).closest('.idea').data('id')
-  var tags = $(e.target).closest('.idea').data('tags').split(" ").filter(function(w){return w})
-  var idea = {body: body, tags: tags}
-  // debugger
-  $.ajax({
-    type: 'PATCH',
-    url: '/api/v1/ideas/' + id,
-    data: {idea: idea}
-  }).success( Idea.reload(id))
-
-}
-
-function updateTitle(e){
-var title = e.target.textContent
-// var id = e.target.parentElement.parentElement.parentElement.parentElement.dataset.id
-var id = $(e.target).closest('.idea').data('id')
-var tags = $(e.target).closest('.idea').data('tags').split(" ").filter(function(w){return w})
-var idea = {title: title, tags: tags}
-
-$.ajax({
-  type: 'PATCH',
-  url: '/api/v1/ideas/' + id,
-  data: {idea: idea}
-}).success(refreshIdeas)
-// debugger
-}
-
-function updateIdea(){
-  var title = $(this.parentElement).find('.title').text()
-  var body  = $(this.parentElement).find('.body').text()
-  var id = this.parentElement.dataset.id
-  var tags = this.parentElement.dataset.tags.split(" ").filter(function(w){return w})
-  var idea = {title: title, body: body, tags: tags}
-
-  $.ajax({
-    type: 'PATCH',
-    url: '/api/v1/ideas/' + id,
-    data: {idea: idea}
-  }).success(refreshIdeas)
-}
-
-function editIdea(){
-  $(this.parentElement).find('.title').prop('contenteditable', 'true').toggleClass('editableField')
-  $(this.parentElement).find('.body').prop('contenteditable', 'true').toggleClass('editableField')
-  $(this.parentElement).find('.ui').toggle()
-}
